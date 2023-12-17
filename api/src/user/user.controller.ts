b4391510c03d03748ac766fc0be0c7f972546990
Luiz Glomyer <glomyerjunior@hotmail.com>
2023-12-16 20:26:41 -0400

    feat: user CRUD

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -12,54 +12,46 @@ import {
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdatePutUserDTO } from './dto/update-put-user.dto';
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+
+  constructor(private readonly userService: UserService) { }
+
   @Post()
-  async create(@Body() { email, name, password }: CreateUserDTO) {
-    return { email, name, password };
+  async create(@Body() data: CreateUserDTO) {
+    return this.userService.create(data);
   }
 
   @Get()
-  async read() {
-    return { users: [] };
+  async list() {
+    return this.userService.list();
   }
 
   @Get(':id')
-  async readOne(@Param('id', ParseIntPipe) id: number) {
-    return { user: {}, id };
+  async show(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.show(id);
   }
 
   @Put(':id')
   async update(
-    @Body() { email, name, password }: UpdatePutUserDTO,
+    @Body() data: UpdatePutUserDTO,
     @Param('id', ParseIntPipe) id: number,
   ) {
-    return {
-      method: 'put',
-      email,
-      name,
-      password,
-      id,
-    };
+    return this.userService.update(id, data);
   }
 
   @Patch(':id')
   async updatePartial(
-    @Body() body: UpdatePatchUserDTO,
+    @Body() data: UpdatePatchUserDTO,
     @Param('id', ParseIntPipe) id: number,
   ) {
-    return {
-      method: 'patch',
-      body,
-      id,
-    };
+    return this.userService.updatePartial(id, data);
   }
 
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number) {
-    return {
-      params: id,
-    };
+    return this.userService.delete(id);
   }
 }
---
+++ api/src/user/user.service.ts
@@ -0,0 +1,81 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { PrismaService } from "src/prisma/prisma.service";
+import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
+
+@Injectable()
+export class UserService {
+
+  constructor(private readonly prisma: PrismaService) { }
+
+  async create(data: CreateUserDTO) {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async list() {
+    return this.prisma.user.findMany();
+  }
+
+  async show(id: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      }
+    });
+  }
+
+  async update(id: number, { email, name, password, birthDate }: UpdatePutUserDTO) {
+    await this.exists(id);
+
+    return this.prisma.user.update({
+      data: {
+        email,
+        name,
+        password,
+        birthDate: birthDate ? new Date(birthDate) : null,
+        updatedAt: new Date()
+      },
+      where: {
+        id
+      }
+    });
+  }
+
+  async updatePartial(id: number, data: UpdatePatchUserDTO) {
+    await this.exists(id);
+
+    if (data.birthDate) {
+      const date = new Date(data.birthDate).toISOString();
+      data.birthDate = String(date);
+    }
+
+    return this.prisma.user.update({
+      data: {
+        ...data,
+        updatedAt: new Date(),
+      },
+      where: {
+        id
+      }
+    });
+  }
+
+  async delete(id: number) {
+    await this.exists(id);
+
+    return this.prisma.user.delete({
+      where: {
+        id
+      }
+    });
+  }
+
+  async exists(id: number) {
+    if (!await this.show(id)) {
+      throw new NotFoundException(`There is no user with the '${id}' id`);
+    }
+  }
+}
